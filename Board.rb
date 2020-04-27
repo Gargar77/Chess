54590ca668ef5f999a1e10c09632dd17b66cd1c7
Gary-Lenovo <gbautista2@knights.ucf.edu>
2020-04-27 18:00:09 -0400

    edited skeleton

--- Board.rb
+++ Board.rb
@@ -1,5 +1,12 @@
 require_relative "Piece"
 
 class Board
+    attr_reader :board
+    def initialize
+      @board = set_board
+    end
+    
 
+    
 end
+
---
+++ Piece.rb
@@ -0,0 +1,27 @@
+class Piece
+    
+    def initialize(pos)
+        @pos = pos
+    end
+end
+class Rook < Piece
+end
+
+class Bishop < Piece
+end
+
+class Queen < piece
+end
+
+class Knight < Piece
+end
+
+class King < Piece
+end
+
+class Pawn < Piece
+end
+
+class NullPiece < Piece
+end
+
